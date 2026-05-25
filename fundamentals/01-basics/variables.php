7c2fe767d406fbe1e64a4d21d191a21076a9f4c7
antonblaise <antoniusblaise@gmail.com>
2026-05-25 15:25:50 +0800

    feat: create `variables.php`

---
+++ fundamentals/01-basics/variables.php
@@ -0,0 +1,14 @@
+<?php
+
+// Do not silently convert data types
+declare(strict_types=1);
+
+$name = "Antonius";
+$age = 28;
+$isDeveloper = true;
+
+echo $name;
+echo "\n";
+echo $age;
+echo "\n";
+echo $isDeveloper;
