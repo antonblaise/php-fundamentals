ca769b638f0361fa4f0c7ee9bbb4fb2232cf7cce
antonblaise <antoniusblaise@gmail.com>
2026-05-25 15:30:47 +0800

    feat: create `strings.php`

---
+++ fundamentals/01-basics/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+$firstName = "Antonius";
+$lastName = "Blaise";
+
+$fullName = $firstName . " " . $lastName;
+
+echo $fullName;
+echo "\n";
+echo "Hello, I am $firstName!";
