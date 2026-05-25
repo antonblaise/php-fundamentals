ba055a28e9148c407e95e1a045d34fd4c230896e
antonblaise <antoniusblaise@gmail.com>
2026-05-25 15:28:43 +0800

    feat: create `data-types.php`

---
+++ fundamentals/01-basics/data-types.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+$string = "Hello";
+$integer = 10;
+$float = 3.14;
+$boolean = true;
+$array = [1, 2, 3];
+$nullValue = null;
+
+// Show the information of the variable
+var_dump($string);
+var_dump($integer);
+var_dump($float);
+var_dump($boolean);
+var_dump($array);
+var_dump($nullValue);
